41b51a052f9b289ee603ee038b6ef230cfebef65
David Leger <david.leger@getkard.com>
2024-12-09 19:27:27 -0400

    feat: hoho 250k

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -1,32 +1,44 @@
-import React from 'react';
-import './ButtonLink.css';
+import React from "react";
+import "./ButtonLink.css";
 
 type Props = {
-    href?: string;
-    disabled?: boolean;
-    external?: boolean;
-    color?: string;
-    textColor?: string;
-    size?: 'small' | 'medium' | 'large';
-    children: React.ReactNode;
-}
+  href?: string;
+  disabled?: boolean;
+  external?: boolean;
+  color?: string;
+  textColor?: string;
+  size?: "small" | "medium" | "large";
+  children: React.ReactNode;
+  variant?: "nav";
+};
 
 const ButtonLink = (props: Props) => {
-    const { external, color = '#eee', textColor = '#111', href, size, disabled } = props;
+  const {
+    external,
+    color = "#eee",
+    textColor = "#111",
+    href,
+    size,
+    variant,
+    disabled,
+  } = props;
 
-    return (
-        <a
-            className="button-link"
-            style={{ '--button-color': color, '--button-text-color': textColor } as any}
-            href={href}
-            target={external ? '_blank' : undefined}
-            rel={external ? 'noopener noreferrer' : undefined}
-            data-size={size}
-            data-disabled={disabled}
-        >
-            {props.children}
-        </a>
-    );
+  return (
+    <a
+      className="button-link"
+      style={
+        { "--button-color": color, "--button-text-color": textColor } as any
+      }
+      href={href}
+      target={external ? "_blank" : undefined}
+      rel={external ? "noopener noreferrer" : undefined}
+      data-size={size}
+      data-disabled={disabled}
+      data-variant={variant}
+    >
+      {props.children}
+    </a>
+  );
 };
 
 export default ButtonLink;
